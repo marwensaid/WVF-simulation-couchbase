4999e80ad902424dbac126cc49ace31d4fa43709
marwensaid <marwensaidi1@gmail.com>
2015-11-20 15:20:44 +0100

    feat(spike wvf) add connection to couchbase , first time -- MS

---
+++ src/main/java/com/orange/wvfSpike/configConnectionDataBase/ConnectToCouchbase.java
@@ -0,0 +1,13 @@
+package com.orange.wvfSpike.configConnectionDataBase;
+
+
+import com.couchbase.client.CouchbaseClient;
+
+/**
+ * Created by msaidi on 11/20/15.
+ */
+public class ConnectToCouchbase {
+    public static CouchbaseClient newCouchbaseClient() {
+        return null;
+    }
+}
