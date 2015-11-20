74d66ed2cf3b31a71d166694f7f8c631f90a3a25
marwensaid <marwensaidi1@gmail.com>
2015-11-20 16:02:01 +0100

    feat(spike wvf) connecting to couchbase-- MS

--- src/main/java/com/orange/wvfSpike/configConnectionDataBase/ConnectToCouchbase.java
+++ src/main/java/com/orange/wvfSpike/configConnectionDataBase/ConnectToCouchbase.java
@@ -3,11 +3,21 @@ package com.orange.wvfSpike.configConnectionDataBase;
 
 import com.couchbase.client.CouchbaseClient;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 /**
  * Created by msaidi on 11/20/15.
  */
 public class ConnectToCouchbase {
-    public static CouchbaseClient newCouchbaseClient() {
-        return null;
+    public static CouchbaseClient newCouchbaseClient() throws URISyntaxException, IOException {
+        ArrayList<URI> uriList = new ArrayList<URI>();
+        uriList.add(new URI("http://localhost:8091/pools"));
+        String bucketName = "default";
+        String password = "";
+        return new CouchbaseClient(uriList, bucketName, password);
     }
+
 }
