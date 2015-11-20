5ad78281d0d65b17925bfb6872ed77ff3a1aaf24
marwensaid <marwensaidi1@gmail.com>
2015-11-20 16:19:52 +0100

    refactor(spike wvf) testing connexion with couchbase-- MS

--- src/test/java/com/orange/wvfSpike/wvfTest/connectionCouchbaseTest.java
+++ src/test/java/com/orange/wvfSpike/wvfTest/connectionCouchbaseTest.java
@@ -13,12 +13,12 @@ import java.util.ArrayList;
 public class connectionCouchbaseTest {
     @Test
     public void connectToCouchbase() throws Exception {
-        ArrayList<URI> urilist = new ArrayList<URI>();
-        urilist.add(new URI("http://localhost:8091/pools"));
+        ArrayList<URI> uriList = new ArrayList<URI>();
+        uriList.add(new URI("http://localhost:8091/pools"));
         String bucketName = "default";
         String password = "";
 
-        CouchbaseClient client = new CouchbaseClient(urilist, bucketName, password);
+        CouchbaseClient client = new CouchbaseClient(uriList, bucketName, password);
 
 
         String key = "wvf_2015-11-20";
