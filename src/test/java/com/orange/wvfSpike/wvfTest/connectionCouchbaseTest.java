ee0defc863bc0a9b1b820a8dc4f7c7555b5972b4
marwensaid <marwensaidi1@gmail.com>
2015-11-20 16:16:35 +0100

    feat(spike wvf) testing connexion with couchbase-- MS

---
+++ src/test/java/com/orange/wvfSpike/wvfTest/connectionCouchbaseTest.java
@@ -0,0 +1,33 @@
+package com.orange.wvfSpike.wvfTest;
+
+import com.couchbase.client.CouchbaseClient;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+/**
+ * Created by msaidi on 11/20/15.
+ */
+public class connectionCouchbaseTest {
+    @Test
+    public void connectToCouchbase() throws Exception {
+        ArrayList<URI> urilist = new ArrayList<URI>();
+        urilist.add(new URI("http://localhost:8091/pools"));
+        String bucketName = "default";
+        String password = "";
+
+        CouchbaseClient client = new CouchbaseClient(urilist, bucketName, password);
+
+
+        String key = "wvf_2015-11-20";
+        String value = "{\n\"titre\":\"couchbase\"\n}";
+
+        client.set(key, value);
+
+        String result = (String) client.get(key);
+        Assert.assertEquals(value, result);
+
+    }
+}
