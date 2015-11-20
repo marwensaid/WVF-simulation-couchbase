2462ef583849d74bd515b666ec360290e496511a
marwensaid <marwensaidi1@gmail.com>
2015-11-20 14:23:39 +0100

    first commit in skipe Wvf with N1QL

---
+++ src/main/java/com/orange/wvfSpike/WvfSpikeApplication.java
@@ -0,0 +1,12 @@
+package com.orange.wvfSpike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvfSpikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WvfSpikeApplication.class, args);
+    }
+}
